bdbdccc8b961384046a207295d013e00b539093b
Erik De Luna <codingerik1.gmail.com>
2020-11-14 17:04:11 -0600

    adding models

---
+++ src/models/todo.model.ts
@@ -0,0 +1,32 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  title?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
